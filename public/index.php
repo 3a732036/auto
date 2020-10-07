a4e8baf31e715fe723f5f8dff5444ea75f623071
Jun-You Ye <3a732036@gm.student.ncut.edu.tw>
2020-10-07 11:15:59 +0800

    index.php changed

--- public/index.php
+++ public/index.php
@@ -9,6 +9,5 @@ use Demo\Hello;
 $world = new World();
 $lara = new Lara();
 $vincent = new Hello\Someone('Vincent');
-// 以下Someone類別的使用可以不用use Demo\Hello;
 $mary = new \Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
